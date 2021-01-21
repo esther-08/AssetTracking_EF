646058d1f1685455f6a5cd8ef47e371127b20c0b
Esther Nampiina <nay.esther08@gmail.com>
2021-01-22 00:10:25 +0100

    Create migration then database

---
+++ AssetTracking_EF/Migrations/20210121225455_InitialCreate.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AssetTracking_EF.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "CompanyAssets",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ModelName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Price = table.Column<int>(type: "int", nullable: false),
+                    PurchaseDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Office = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CompanyAssets", x => x.ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CompanyAssets");
+        }
+    }
+}
--- AssetTracking_EF/Program.cs
+++ AssetTracking_EF/Program.cs
@@ -113,7 +113,7 @@ namespace AssetTracking_EF
         public DbSet<CompanyAsset> CompanyAssets { get; set; }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer(@"Data Source=DESKTOP-Q3LBSV4\SQLEXPRESS;Initial Catalog=AssetTrackor;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False")
+            optionsBuilder.UseSqlServer(@"Data Source=DESKTOP-Q3LBSV4\SQLEXPRESS;Initial Catalog=AssetTrackor;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
         }
     }
     
