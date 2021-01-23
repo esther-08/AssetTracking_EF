ced02f17b5f8a6d1616a11399d449ff8e0c4ca3a
Esther Nampiina <nay.esther08@gmail.com>
2021-01-24 00:02:22 +0100

    Added AssetType and ID properties to company asset base class

--- AssetTracking_EF/CompanyAsset.cs
+++ AssetTracking_EF/CompanyAsset.cs
@@ -11,6 +11,7 @@ namespace AssetTracking_EF
             ModelName = modelName;
             Price = price;
             PurchaseDate = purchaseDate;
+            AssetType = "Laptop";
         }
 
     }
@@ -21,6 +22,7 @@ namespace AssetTracking_EF
             ModelName = modelName;
             Price = price; // in dollars
             PurchaseDate = purchaseDate;
+            AssetType = "Phone";
         }
     }
     class CompanyAsset
@@ -28,12 +30,14 @@ namespace AssetTracking_EF
         public CompanyAsset(string modelName, int price, DateTime purchaseDate, string office)
         {
 
+            AssetType = "";
             ModelName = modelName;
             Price = price;
             PurchaseDate = purchaseDate;
             Office = office;
         }
         public int ID { get; set; }
+        public string AssetType { get; set; }
         public string ModelName { get; set; }
         public int Price { get; set; }
         public DateTime PurchaseDate { get; set; }
