7e2019d3add60b5924bb6efbd0eb72ea47c9035b
Esther Nampiina <nay.esther08@gmail.com>
2021-01-24 00:08:05 +0100

    Changed program code to use entity framework

--- AssetTracking_EF/Program.cs
+++ AssetTracking_EF/Program.cs
@@ -2,86 +2,106 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Globalization;
-using Microsoft.EntityFrameworkCore;
 
 namespace AssetTracking_EF
 {
     class Program
     {
+        static AssetTrackorContext _assetTrackorContext = new AssetTrackorContext();
+        //private static IEnumerable<CompanyAsset> assets;
+
         static void Main(string[] args)
         {
-            // Level 1
-            List<CompanyAsset> myAssets = new List<CompanyAsset>();
+            // Level 1 - Create
             LaptopComputer pc1 = new LaptopComputer("MacBook", 3341, new DateTime(2018, 12, 12), "Uganda");
             LaptopComputer pc2 = new LaptopComputer("Asus", 2950, new DateTime(2018, 1, 12), "Sweden");
             LaptopComputer pc3 = new LaptopComputer("Lenovo", 999, new DateTime(2019, 12, 12), "USA");
 
-            List<MobilePhone> myPhones = new List<MobilePhone>();
             MobilePhone phone1 = new MobilePhone("Iphone", 1099, new DateTime(2011, 12, 12), "Uganda");
             MobilePhone phone2 = new MobilePhone("Samsung", 1299, new DateTime(2018, 3, 12), "USA");
-            MobilePhone phone3 = new MobilePhone("Nokia", 699, new DateTime(2018, 4, 12), "Sweden");
+            MobilePhone phone3 = new MobilePhone("Nokia", 699, new DateTime(2018, 4, 12), "Sweden");            
 
-            myAssets.Add(pc1);
-            myAssets.Add(phone1);
-            myAssets.Add(pc3);
-            myAssets.Add(phone2);
-            myAssets.Add(pc2);
-            myAssets.Add(phone3);
+            _assetTrackorContext.CompanyAssets.Add(pc1);
+            _assetTrackorContext.SaveChanges();
 
-            // Level 2
-            myAssets = myAssets.OrderBy(myAssets => myAssets.GetType().ToString()).ToList();
+            _assetTrackorContext.CompanyAssets.Add(phone1);
+            _assetTrackorContext.SaveChanges();
+
+            _assetTrackorContext.CompanyAssets.Add(pc3);
+            _assetTrackorContext.SaveChanges();
+
+            _assetTrackorContext.CompanyAssets.Add(phone2);
+            _assetTrackorContext.SaveChanges();
+
+            _assetTrackorContext.CompanyAssets.Add(pc2);
+            _assetTrackorContext.SaveChanges();
+
+            _assetTrackorContext.CompanyAssets.Add(phone3);
+            _assetTrackorContext.SaveChanges();
+
+
+            // Level 2 - Read and Report
+            var query = from item in _assetTrackorContext.CompanyAssets 
+                        orderby item.AssetType ascending
+                        select item;
 
             Console.WriteLine("\nLEVEL2 \nOrder by Type");
-            Console.WriteLine("ModelName   Price  PurchaseDate Location");
-            foreach (CompanyAsset asset in myAssets)
+            WriteHeadingToConsole();
+            foreach (var item in query)
             {
-                Console.WriteLine(asset.ModelName.PadRight(10) + asset.Price.ToString().PadLeft(7) + "  " + asset.PurchaseDate.ToString("MM/dd/yyyy").PadRight(10) + "   " + asset.Office.PadRight(10));
+                WriteItemToConsole(item);
             }
 
-            myAssets = myAssets.OrderBy(myAssets => myAssets.PurchaseDate).ToList();
+            
+            query = from item in _assetTrackorContext.CompanyAssets
+                    orderby item.PurchaseDate ascending
+                    select item;
 
             Console.WriteLine("\nOrder by Purchase Date");
-            Console.WriteLine("ModelName   Price  PurchaseDate Location");
+            WriteHeadingToConsole();
             DateTime nowDate = DateTime.Now;
             TimeSpan assetLifeSpan;
 
-            foreach (CompanyAsset asset in myAssets)
+            foreach (var item in query)
             {
-                assetLifeSpan = nowDate - asset.PurchaseDate;
+                assetLifeSpan = nowDate - item.PurchaseDate;
                 ColorConsole(assetLifeSpan);
-                Console.WriteLine(asset.ModelName.PadRight(10) + asset.Price.ToString().PadLeft(7) + "  " + asset.PurchaseDate.ToString("MM/dd/yyyy").PadRight(10) + "   " + asset.Office.PadRight(10));
+                WriteItemToConsole(item);
+                
             }
 
-            // Level 3
-            myAssets = myAssets.OrderBy(myAssets => myAssets.Office).ToList();
+            // Level 3 - Read and Report           
+            query = from item in _assetTrackorContext.CompanyAssets
+                    orderby item.Office ascending
+                    select item;
+                   
             TimeSpan soonEoLSpan6 = new TimeSpan(1005, 0, 0, 0, 0);
-
             Console.WriteLine("\nLEVEL3 \nOrder by Office");
-            Console.WriteLine("ModelName        Price     PurchaseDate Location");
+            WriteHeadingToConsole();
             CultureInfo cultureInfo = new CultureInfo("en-US");
             string price;
-            foreach (CompanyAsset asset in myAssets)
+            foreach (var item in query)
             {
-                assetLifeSpan = nowDate - asset.PurchaseDate;
+                assetLifeSpan = nowDate - item.PurchaseDate;
 
-                if (asset.Office == "Sweden")
+                if (item.Office == "Sweden")
                 {
                     // Convert dollars to SEK first
-                    price = (asset.Price * 8.23).ToString("C", CultureInfo.CreateSpecificCulture("sv-SE"));
+                    price = (item.Price * 8.23).ToString("C", CultureInfo.CreateSpecificCulture("sv-SE"));
                 }
-                else if (asset.Office == "Uganda")
+                else if (item.Office == "Uganda")
                 {
                     // Convert dollars to Ugx first
-                    price = (asset.Price * 3700).ToString("C", CultureInfo.CreateSpecificCulture("en-UG"));
+                    price = (item.Price * 3700).ToString("C", CultureInfo.CreateSpecificCulture("en-UG"));
                 }
                 else
                 {
                     // Default price in dollars
-                    price = asset.Price.ToString("C", CultureInfo.CreateSpecificCulture("en-US"));
+                    price = item.Price.ToString("C", CultureInfo.CreateSpecificCulture("en-US"));
                 }
 
                 ColorConsole(assetLifeSpan);
-                Console.WriteLine(asset.ModelName.PadRight(10) + price.PadLeft(15) + "  " + asset.PurchaseDate.ToString("MM/dd/yyyy").PadRight(10) + "   " + asset.Office.PadRight(10));
+                WriteItemToConsole(item);                
             }
 
             Console.ReadLine();
@@ -89,9 +109,9 @@ namespace AssetTracking_EF
 
         static void ColorConsole(TimeSpan assetLifeSpan)
         {
-            // 1005 is 2 years and 9 months  = (365*3)-(30*3) days
+            //1005 is 2 years and 9 months  = (365*3)-(30*3) days
             TimeSpan soonEoLSpan3 = new TimeSpan(1005, 0, 0, 0, 0);
-            // 915 is 2 years and 6 months  = (365*3)-(30*6) days
+            //915 is 2 years and 6 months  = (365*3)-(30*6) days
             TimeSpan soonEoLSpan6 = new TimeSpan(915, 0, 0, 0, 0);
             if (assetLifeSpan > soonEoLSpan3)
             {
@@ -106,15 +126,16 @@ namespace AssetTracking_EF
                 Console.ForegroundColor = ConsoleColor.White;
             }
         }
-    }
 
-    internal class AssetTrackorContext : DbContext
-    {
-        public DbSet<CompanyAsset> CompanyAssets { get; set; }
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        static void WriteHeadingToConsole()
         {
-            optionsBuilder.UseSqlServer(@"Data Source=DESKTOP-Q3LBSV4\SQLEXPRESS;Initial Catalog=AssetTrackor;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+            Console.WriteLine("Type        ModelName   Price  PurchaseDate Location");
         }
+        static void WriteItemToConsole(CompanyAsset item)
+        {
+            Console.WriteLine(item.AssetType.PadRight(10) + "  " + item.ModelName.PadRight(10) + item.Price.ToString().PadLeft(7) + "  " + item.PurchaseDate.ToString("MM/dd/yyyy").PadRight(10) + "   " + item.Office.PadRight(10));
+        }
+
     }
     
 }
