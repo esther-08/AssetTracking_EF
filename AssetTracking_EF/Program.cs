8b25a6024aa2da314683c5099e15d1a0ca3fdcb0
Esther Nampiina <nay.esther08@gmail.com>
2021-01-24 01:29:46 +0100

    Move create and read to their own methods

--- AssetTracking_EF/Program.cs
+++ AssetTracking_EF/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Globalization;
 
@@ -12,97 +11,11 @@ namespace AssetTracking_EF
         static void Main(string[] args)
         {
             // Level 1 - Create
-            LaptopComputer pc1 = new LaptopComputer("MacBook", 3341, new DateTime(2018, 12, 12), "Uganda");
-            LaptopComputer pc2 = new LaptopComputer("Asus", 2950, new DateTime(2018, 1, 12), "Sweden");
-            LaptopComputer pc3 = new LaptopComputer("Lenovo", 999, new DateTime(2019, 12, 12), "USA");
-
-            MobilePhone phone1 = new MobilePhone("Iphone", 1099, new DateTime(2011, 12, 12), "Uganda");
-            MobilePhone phone2 = new MobilePhone("Samsung", 1299, new DateTime(2018, 3, 12), "USA");
-            MobilePhone phone3 = new MobilePhone("Nokia", 699, new DateTime(2018, 4, 12), "Sweden");            
-
-            _assetTrackorContext.CompanyAssets.Add(pc1);
-            _assetTrackorContext.SaveChanges();
-
-            _assetTrackorContext.CompanyAssets.Add(phone1);
-            _assetTrackorContext.SaveChanges();
-
-            _assetTrackorContext.CompanyAssets.Add(pc3);
-            _assetTrackorContext.SaveChanges();
-
-            _assetTrackorContext.CompanyAssets.Add(phone2);
-            _assetTrackorContext.SaveChanges();
-
-            _assetTrackorContext.CompanyAssets.Add(pc2);
-            _assetTrackorContext.SaveChanges();
-
-            _assetTrackorContext.CompanyAssets.Add(phone3);
-            _assetTrackorContext.SaveChanges();
-
+            Create();
 
             // Level 2 - Read and Report
-            var query = from item in _assetTrackorContext.CompanyAssets 
-                        orderby item.AssetType ascending
-                        select item;
-
-            Console.WriteLine("\nLEVEL2 \nOrder by Type");
-            WriteHeadingToConsole();
-            foreach (var item in query)
-            {
-                WriteItemToConsole(item, "");
-            }
-
-            
-            query = from item in _assetTrackorContext.CompanyAssets
-                    orderby item.PurchaseDate ascending
-                    select item;
-
-            Console.WriteLine("\nOrder by Purchase Date");
-            WriteHeadingToConsole();
-            DateTime nowDate = DateTime.Now;
-            TimeSpan assetLifeSpan;
-
-            foreach (var item in query)
-            {
-                assetLifeSpan = nowDate - item.PurchaseDate;
-                ColorConsole(assetLifeSpan);
-                WriteItemToConsole(item, "");
-                
-            }
-
-            // Level 3 - Read and Report           
-            query = from item in _assetTrackorContext.CompanyAssets
-                    orderby item.Office ascending
-                    select item;
-                   
-            Console.WriteLine("\nLEVEL3 \nOrder by Office");
-            WriteHeadingToConsole();
-            string price;
-            foreach (var item in query)
-            {
-                assetLifeSpan = nowDate - item.PurchaseDate;
-
-                if (item.Office == "Sweden")
-                {
-                    // Convert dollars to SEK first
-                    price = (item.Price * 8.23).ToString("C", CultureInfo.CreateSpecificCulture("sv-SE"));
-                }
-                else if (item.Office == "Uganda")
-                {
-                    // Convert dollars to Ugx first
-                    price = (item.Price * 3700).ToString("C", CultureInfo.CreateSpecificCulture("en-UG"));
-                }
-                else
-                {
-                    // Default price in dollars
-                    price = item.Price.ToString("C", CultureInfo.CreateSpecificCulture("en-US"));
-                }
-
-                ColorConsole(assetLifeSpan);
-
-                // Update price before printing on console
-                WriteItemToConsole(item, price);                
-            }
-
+            ReadAndReport();
+          
             Console.ReadLine();
         }
 
@@ -138,13 +51,101 @@ namespace AssetTracking_EF
                 price = item.Price.ToString();
             }
             Console.WriteLine(
-                item.AssetType.PadRight(10) + "  " 
-                + item.ModelName.PadRight(11) 
-                + price.PadLeft(14) + "  " 
-                + item.PurchaseDate.ToString("MM/dd/yyyy").PadRight(10) + "   " 
+                item.AssetType.PadRight(10) + "  "
+                + item.ModelName.PadRight(11)
+                + price.PadLeft(14) + "  "
+                + item.PurchaseDate.ToString("MM/dd/yyyy").PadRight(10) + "   "
                 + item.Office.PadRight(10));
         }
+        static void Create()
+        {
+            LaptopComputer pc1 = new LaptopComputer("MacBook", 3341, new DateTime(2018, 12, 12), "Uganda");
+            LaptopComputer pc2 = new LaptopComputer("Asus", 2950, new DateTime(2018, 1, 12), "Sweden");
+            LaptopComputer pc3 = new LaptopComputer("Lenovo", 999, new DateTime(2019, 12, 12), "USA");
 
+            MobilePhone phone1 = new MobilePhone("Iphone", 1099, new DateTime(2011, 12, 12), "Uganda");
+            MobilePhone phone2 = new MobilePhone("Samsung", 1299, new DateTime(2018, 3, 12), "USA");
+            MobilePhone phone3 = new MobilePhone("Nokia", 699, new DateTime(2018, 4, 12), "Sweden");
+
+            _assetTrackorContext.CompanyAssets.Add(pc1);
+            _assetTrackorContext.SaveChanges();
+
+            _assetTrackorContext.CompanyAssets.Add(phone1);
+            _assetTrackorContext.SaveChanges();
+
+            _assetTrackorContext.CompanyAssets.Add(pc3);
+            _assetTrackorContext.SaveChanges();
+
+            _assetTrackorContext.CompanyAssets.Add(phone2);
+            _assetTrackorContext.SaveChanges();
+
+            _assetTrackorContext.CompanyAssets.Add(pc2);
+            _assetTrackorContext.SaveChanges();
+
+            _assetTrackorContext.CompanyAssets.Add(phone3);
+            _assetTrackorContext.SaveChanges();
+        }
+        static void ReadAndReport()
+        {
+            var query = from item in _assetTrackorContext.CompanyAssets
+                        orderby item.AssetType ascending
+                        select item;
+
+            Console.WriteLine("\nLEVEL2 \nOrder by Type");
+            WriteHeadingToConsole();
+            foreach (var item in query)
+            {
+                WriteItemToConsole(item, "");
+            }
+
+            query = from item in _assetTrackorContext.CompanyAssets
+                    orderby item.PurchaseDate ascending
+                    select item;
+
+            Console.WriteLine("\nOrder by Purchase Date");
+            WriteHeadingToConsole();
+            DateTime nowDate = DateTime.Now;
+            TimeSpan assetLifeSpan;
+
+            foreach (var item in query)
+            {
+                assetLifeSpan = nowDate - item.PurchaseDate;
+                ColorConsole(assetLifeSpan);
+                WriteItemToConsole(item, "");
+            }
+
+            // Level 3 - Read and Report           
+            query = from item in _assetTrackorContext.CompanyAssets
+                    orderby item.Office ascending
+                    select item;
+
+            Console.WriteLine("\nLEVEL3 \nOrder by Office");
+            WriteHeadingToConsole();
+            string price;
+            foreach (var item in query)
+            {
+                assetLifeSpan = nowDate - item.PurchaseDate;
+
+                if (item.Office == "Sweden")
+                {
+                    // Convert dollars to SEK first
+                    price = (item.Price * 8.23).ToString("C", CultureInfo.CreateSpecificCulture("sv-SE"));
+                }
+                else if (item.Office == "Uganda")
+                {
+                    // Convert dollars to Ugx first
+                    price = (item.Price * 3700).ToString("C", CultureInfo.CreateSpecificCulture("en-UG"));
+                }
+                else
+                {
+                    // Default price in dollars
+                    price = item.Price.ToString("C", CultureInfo.CreateSpecificCulture("en-US"));
+                }
+
+                ColorConsole(assetLifeSpan);
+                WriteItemToConsole(item, price);
+            }
+
+        }
     }
-    
 }
